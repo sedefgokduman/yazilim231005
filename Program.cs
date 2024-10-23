9200615f3328034fae1efd994d80cf2e3fd94b07
sedef <sedef@SEDEF>
2024-10-23 20:53:23 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazilim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı girin");
+            int sayi=Convert.ToInt16(Console.ReadLine());
+            int yuzde = sayi * 18 / 100;
+            Console.WriteLine($"girdiğiniz sayının %18'i {yuzde}'dir.");
+            Console.ReadKey();
+        }
+    }
+}
